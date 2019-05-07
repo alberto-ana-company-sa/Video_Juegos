4e32ba04d3eb25dfe050accd6ddd9bd113b714f6
AnabelCarnerero <anabelcb00@gmail.com>
2019-05-07 23:13:53 +0200

    asd

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -15,6 +15,7 @@ namespace Proyecto
         static void Main()
         {
             Application.EnableVisualStyles();
+
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
